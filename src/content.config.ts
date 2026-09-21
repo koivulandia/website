952cf9601df6206cae7389be5a064a1af5b7900d
Fabian Mäkilä <contact@fabianadrian.fi>
2026-09-21 19:49:45 +0300

    fixes and functionality

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
 import { defineCollection } from 'astro:content';
-import { glob, file } from 'astro/loaders';
+import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const legislations = defineCollection({
@@ -28,14 +28,17 @@ const literature = defineCollection({
 });
 
 const news = defineCollection({
-  loader: glob({ base: './src/content/news', pattern: '**/*.{md,mdx}' }),
+  loader: glob({ base: './src/content/news', pattern: '**/*.{md,mdx}', generateId: ({ entry }) => {
+      const filename = entry.split('/').pop()!;
+      return filename.replace(/\.(md|mdx)$/, '');
+    },
+ }),
   schema: z.object({
     title: z.string(),
     cover: z.object({
       src: z.string(),
       alt: z.string()
-    }).optional(),
-    date: z.coerce.date()
+    }).optional()
   }),
 });
 
